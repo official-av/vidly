4a855905b3f0459358127f6a50d75836fc1bb428
Anmol Vashistha <anmol.vashistha@newgen.co.in>
2018-10-12 12:59:50 +0530

    fundamentals- custom routes and attribute routres

--- vidly/vidly/Controllers/MovieController.cs
+++ vidly/vidly/Controllers/MovieController.cs
@@ -16,7 +16,31 @@ namespace vidly.Controllers
             {
                 Name = "Shrek"
             };
-            return View(Movie);
+            //return View(Movie);
+            //return Content("hello world");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            return RedirectToAction("Index", "Home",new { page=1,sortby="name"});
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        //for movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "name";
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+        [Route("movies/released/{year:regex(\\d{4})}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
         }
     }
 }
