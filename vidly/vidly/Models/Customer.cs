bc8cf23f58076bbb20addd6f13c6cd4dd6dd2d92
Anmol Vashistha <anmol.vashistha@newgen.co.in>
2018-10-18 12:22:46 +0530

    Added migrations and membershiptype

--- vidly/vidly/Models/Customer.cs
+++ vidly/vidly/Models/Customer.cs
@@ -10,5 +10,8 @@ namespace vidly.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool IsSubscribedToNewsLetter { get; set; }
+        public MembershipType MembershipType { get; set; } //isNavigationProperty
+        public byte MembershipTypeId { get; set; }
     }
 }
